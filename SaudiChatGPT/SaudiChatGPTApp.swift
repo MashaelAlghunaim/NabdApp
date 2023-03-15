5c6f419f07d11ad00304269aa1b32072ff879bac
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2023-03-15 21:07:29 +0300

    Initial Commit

---
+++ SaudiChatGPT/SaudiChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaudiChatGPTApp.swift
+//  SaudiChatGPT
+//
+//  Created by Mashael Alghunaim on 23/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SaudiChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
